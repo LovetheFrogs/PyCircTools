aabf497205b3ef837d114f0c2503c9beefacd9a5
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-20 00:14:15 +0200

    Implemented XOR gate

---
+++ BinaryTools/LogicGates/Xor.py
@@ -0,0 +1,42 @@
+class Xor:
+    def __init__(self, inputNumber=2):
+        self.input = self.__create_input(inputNumber)
+        self.output = False
+        self.numOfInputs = inputNumber
+
+    def get_input(self, num):
+        return self.input[num]
+
+    def get_output(self):
+        return self.output
+
+    def get_numOfInputs(self):
+        return self.numOfInputs
+
+    def set_input(self, num, value):
+        self.input[num] = value
+        self.__calculate_output()
+        return self
+
+    def add_input(self):
+        self.input.append(False)
+        self.numOfInputs += 1
+        self.__calculate_output()
+        return self
+
+    def remove_input(self):
+        self.input.pop()
+        self.numOfInputs -= 1
+        self.__calculate_output()
+        return self
+
+    def __calculate_output(self):
+        output = False
+        for value in self.input:
+            output = ((output and not value) or (not output and value))
+        self.output = output
+        return self
+
+    @staticmethod
+    def __create_input(number):
+        return [False] * number
