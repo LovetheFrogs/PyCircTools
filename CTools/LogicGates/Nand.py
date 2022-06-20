f2b6b09ddef60a2d63185bb6f574eee7820cc129
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-20 18:52:07 +0200

    Added NAND gate docstring

--- CTools/LogicGates/Nand.py
+++ CTools/LogicGates/Nand.py
@@ -3,7 +3,17 @@ from CTools.Exceptions.LogicGateExceptions import NonPositiveInput, NotAnInput
 
 
 class Nand:
+    """
+    Nand logic gate. Can have any number of inputs greater or equal than 1.
+    """
     def __init__(self, inputNumber=2):
+        """
+        Nand class constructor method.
+
+        :param inputNumber: Number of inputs for the gate. Defaults to two.
+        :type inputNumber: int
+        :raises NonPositiveInput: Raised when inputNumber is lower or equal to zero.
+        """
         if inputNumber <= 0:
             raise NonPositiveInput
 
@@ -12,18 +22,49 @@ class Nand:
         self.numOfInputs = inputNumber
 
     def get_input(self, num):
+        """
+        Method get_input is used to get the value of input[num].
+
+        :param num: Position of the input you want to get the value of.
+        :type num: int
+        :raises NotAnInput: Raised when the selected input does not exist.
+        :return: Returns the value of the input num.
+        :rtype: bool
+        """
         if num >= self.numOfInputs:
             raise NotAnInput
 
         return self.input[num]
 
     def get_output(self):
+        """
+        Method get_output is used to get the output of a logic gate.
+
+        :return: Output of the gate.
+        :rtype: bool
+        """
         return self.output
 
     def get_numOfInputs(self):
+        """
+        Method get_numOfInputs is used to get the number of inputs of a logic gate.
+
+        :return: Number of inputs
+        :rtype: int
+        """
         return self.numOfInputs
 
     def set_input(self, num, value):
+        """
+        Method set_input sets a certain input to the desired value, either True or False.
+
+        :param num: Number of the input selected.
+        :type num: int
+        :param value: Desired value of the input.
+        :type value: bool
+        :raises NotAnInput: Raised when the selected input does not exist.
+        :raises NotTruthValue: Raised when value's type is not bool.
+        """
         if num >= self.numOfInputs:
             raise NotAnInput
 
@@ -42,12 +83,18 @@ class Nand:
         return self
 
     def add_input(self):
+        """
+        Method add_input adds a new input to a logic gate.
+        """
         self.input.append(False)
         self.numOfInputs += 1
         self.__calculate_output()
         return self
 
     def remove_input(self):
+        """
+        Removes the last input of a logic gate.
+        """
         self.input.pop()
         self.__calculate_output()
         self.numOfInputs -= 1
--- a.py
+++
@@ -1,9 +0,0 @@
-from CTools.LogicGates import *
-
-
-a = And.__init__()
-a.get_input()
-a.get_output()
-a.set_input()
-raise NotAnInput
-no = Nor.__init__().get_input()
