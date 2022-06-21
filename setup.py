a9ccda7dc061bfaa583ff44c873d962fdcdf6c6c
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-21 16:28:47 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CTools',
+    packages=find_packages(include=['Exceptions', 'LogicGates']),
+    version='beta-0.0.1',
+    description='CTools, a python circuit library.',
+    author='LovetheFrogs',
+    license='GPL-3.0'
+)
