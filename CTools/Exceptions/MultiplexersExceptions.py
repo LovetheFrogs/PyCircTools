8288f77b02de8777049fb9c51f05a82c68953168
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-23 16:33:32 +0200

    Added new Exception to Multiplexers
    Added the NonExistingControlSignal Exception

--- CTools/Exceptions/MultiplexersExceptions.py
+++ CTools/Exceptions/MultiplexersExceptions.py
@@ -22,3 +22,15 @@ class NonExistingInput(MultiplexerException):
         else:
             self.msg = "Input requested does not exist."
         super().__init__(self.msg)
+
+
+class NonExistingControlSignal(MultiplexerException):
+    """
+    NonExistingControlSignal is raised when a Multiplexer/Demultiplexer doesn't have the set control signal asked for.
+    """
+    def __init__(self, requestedInput=None):
+        if requestedInput is not None:
+            self.msg = "Set control signal " + requestedInput + " does not exist."
+        else:
+            self.msg = "Set control signal does not exist."
+        super().__init__(self.msg)
