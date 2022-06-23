237d0fd31ed40965d86caffe72a300d1f2190d29
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-24 00:38:16 +0200

    Added comments for Mux16to1.py

--- CTools/Multiplexers/Mux16to1.py
+++ CTools/Multiplexers/Mux16to1.py
@@ -3,27 +3,67 @@ from CTools.Exceptions.CircuitToolsExceptions import NotTruthValue
 
 
 class Mux16to1:
+    """
+    16 to 1 Multiplexer. Takes four bits as Set input.
+    """
     def __init__(self):
+        """
+        Mux16to1 class constructor method.
+        """
         self.input = [False] * 16
         self.set = [False] * 4
         self.output = self.__calculate_output()
 
     def get_input(self, num):
+        """
+        Method get_input is used to get the value of input[num]
+
+        :param num: Number of the input you want to get. It is a number from 0 to 15.
+        :type num: int
+        :raises NonExistingInput: Raised when a Multiplexer/Demultiplexer doesn't have the input asked for.
+        :return: Value of the desired input (num).
+        :rtype: bool
+        """
         if num >= len(self.input) or num < 0:
             raise NonExistingInput(num)
 
         return self.input[num]
 
     def get_set(self, setNum):
+        """
+        Method get_set gets the value of the set[setNum] control signal.
+
+        :param setNum: Number of the set control signal you want to get, ranges from 0 to 4.
+        :type setNum: int
+        :raises NonExistingControlSignal: Raised when a Multiplexer/Demultiplexer doesn't have setNum control signal.
+        :return: Value of the desired set control signal (setNum).
+        :rtype: bool
+        """
         if setNum >= len(self.set) or setNum < 0:
             raise NonExistingControlSignal
 
         return self.set[setNum]
 
     def get_output(self):
+        """
+        Method get_output gets the output of the 16 to 1 Multiplexer.
+
+        :return: Value of the output.
+        :rtype: bool
+        """
         return self.output
 
     def set_input(self, num, value):
+        """
+        Method set_input sets a certain input to the desired value, either True or False.
+
+        :param num: Number of the input selected.
+        :type num: int
+        :param value: Desired value of the input.
+        :type value: bool
+        :raises NonExistingInput: raised when a Multiplexer/Demultiplexer doesn't have the input asked for.
+        :raises NotTruthValue: Raised when value's type is not bool.
+        """
         if num >= len(self.input) or num < 0:
             raise NonExistingInput(num)
 
@@ -35,6 +75,16 @@ class Mux16to1:
         return self
 
     def set_set(self, setNum, value):
+        """
+        Method set_set sets the value of the set control signal to the desired truth value.
+
+        :param setNum: Number of the set control signal selected.
+        :type setNum: int
+        :param value: Desired value of the set control signal.
+        :type value: bool
+        :raises NonExistingControlSignal: Raised when a Multiplexer/Demultiplexer doesn't have setNum control signal.
+        :raises NotTruthValue: Raised when value's type is not bool.
+        """
         if setNum >= len(self.set) or setNum < 0:
             raise NonExistingControlSignal(setNum)
 
@@ -46,6 +96,12 @@ class Mux16to1:
         return self
 
     def __calculate_output(self):
+        """
+        Private method __calculate_output calculates the output signal using the 16-to-1 mux formula.
+
+        :return: Value of the output signal.
+        :rtype: bool
+        """
         first = not self.set[3] and not self.set[2] and not self.set[1] and not self.set[0] and self.input[0]
         second = not self.set[3] and not self.set[2] and not self.set[1] and self.set[0] and self.input[1]
         third = not self.set[3] and not self.set[2] and self.set[1] and not self.set[0] and self.input[2]
