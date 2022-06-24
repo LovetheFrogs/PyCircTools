20a904bad92b88b3a35a1009a0039e1239119702
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-24 19:31:47 +0200

    Updated setup for v0.1.0

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ this_directory = Path(__file__).parent
 desc = (this_directory / "README.md").read_text()
 setup(
     name='PyCircTools',
-    packages=find_packages(include=['Exceptions', 'LogicGates']),
-    version='0.0.1',
-    description='CTools, a python circuit library.',
+    packages=find_packages(include=['Exceptions', 'LogicGates', 'Multiplexers']),
+    version='0.1.0',
+    description='PyCircTools, a python circuit library.',
     author='LovetheFrogs',
     license='GPL-3.0',
     url='https://github.com/LovetheFrogs/CTools',
