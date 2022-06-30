b6e595e09dc7b3d50310ccd2437a70c38a2fa7bc
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-06-30 15:11:40 +0200

    Added comments for the DLatch class

--- PyCircTools/Latches/DLatch.py
+++ PyCircTools/Latches/DLatch.py
@@ -3,25 +3,62 @@ from PyCircTools.LogicGates import And, Nor, Not
 
 
 class DLatch:
+    """
+    D-Latch module. Takes 2 inputs (Data and enable).
+    """
     def __init__(self):
+        """
+        DLatch class constructor. Initialises all inputs and outputs to False.
+        """
         self.D = False
         self.enable = False
         self.Q = False
         self.Qp = False
 
     def get_D(self):
+        """
+        Method get_D gets the value of the Data input.
+
+        :return: Value of the latch's Data input.
+        :rtype: bool
+        """
         return self.D
 
     def get_enable(self):
+        """
+        Method get_enable gets the value of the enable input.
+
+        :return: Value of the latch's Enable input.
+        :rtype: bool
+        """
         return self.enable
 
     def get_Q(self):
+        """
+        Method get_Q returns the value of the Q output.
+
+        :return: Value of the latch's Q output.
+        :rtype: bool
+        """
         return self.Q
 
     def get_Qp(self):
+        """
+        Method get_Qp returns the value of the Qp output.
+
+        :return: Value of the latch's Qp output.
+        :rtype: bool
+        """
         return self.Qp
 
     def set_D(self, value):
+        """
+        Method set_D sets the value of the Data input to the bool value.
+
+        :param value: Desired value of the latch's Data input.
+        :type value: bool
+        :raises NotTruthValue: Raised when a variable type is not bool.
+        """
         if type(value) is not bool:
             raise NotTruthValue
 
@@ -30,6 +67,13 @@ class DLatch:
         return self
 
     def set_enable(self, value):
+        """
+        Method set_enable sets the value of Enable to the bool value.
+
+        :param value: Desired value of the latch's Enable input.
+        :type value: bool
+        :raises NotTruthValue: Raised when a variable type is not bool.
+        """
         if type(value) is not bool:
             raise NotTruthValue
 
@@ -38,6 +82,9 @@ class DLatch:
         return self
 
     def __calculate_output(self):
+        """
+        Method __calculate_output calculates the output of both the Q and Qp signals.
+        """
         and_d = And().set_input(0, self.enable).set_input(1, self.D)
         not_d = Not().set_input(self.D)
         and_not_d = And().set_input(0, not_d.get_output()).set_input(1, self.enable)
