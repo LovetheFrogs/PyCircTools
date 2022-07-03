5452be3c44529c0af0e3e50350a838cdcaac99ed
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-03 16:38:26 +0200

    Tested DLatch

--- tests/Latches_tests/test_DLatch.py
+++ tests/Latches_tests/test_DLatch.py
@@ -10,3 +10,15 @@ def test_DLatch():
 
     if latch.set_D(True).get_Q() and not latch.get_Qp():
         errors.append("Outputs changed when enable is still False!")
+
+    if not latch.set_enable(True).set_D(False).get_Q() and latch.get_Qp():
+        pass
+    else:
+        errors.append("Data False does not give back expected output!")
+
+    if latch.set_D(True).get_Q() and not latch.get_Qp():
+        pass
+    else:
+        errors.append("Data True does not give back expected output!")
+
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
