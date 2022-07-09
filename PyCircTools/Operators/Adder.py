6897dfac4e98af975730e04d24d31c08f6d416ae
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-09 23:53:38 +0200

    Implemented adder
    Implemented the Adder class, which can be used to create both half and full adders

--- PyCircTools/Operators/Adder.py
+++ PyCircTools/Operators/Adder.py
@@ -1,3 +1,4 @@
+from PyCircTools import NotTruthValue, And, Xor, Or
 from PyCircTools.Exceptions.OperatorsExceptions import NotCorrectAdder
 
 
@@ -25,8 +26,61 @@ class Adder:
 
     def get_carryIn(self):
         if not self.full:
-            raise(NotCorrectAdder('get_carryIn', False))
+            raise NotCorrectAdder('get_carryIn', False)
 
         return self.carryOut
 
-    # Add method to convert to full adder from half adder.
+    def set_A(self, value):
+        if type(value) is not bool:
+            raise NotTruthValue
+
+        self.A = value
+        self.__calculate_output()
+        return self
+
+    def set_B(self, value):
+        if type(value) is not bool:
+            raise NotTruthValue
+
+        self.B = value
+        self.__calculate_output()
+        return self
+
+    def set_carryIn(self, value):
+        if not self.full:
+            raise NotCorrectAdder('set_carryIn', False)
+
+        if type(value) is not bool:
+            raise NotTruthValue
+
+        self.carryIn = value
+        self.__calculate_output()
+        return self
+
+    def convert(self):
+        if not self.full:
+            self.full = not self.full
+            self.carryIn = False
+            self.__calculate_output()
+        else:
+            self.full = not self.full
+            self.__delattr__(self.carryIn)
+            self.__calculate_output()
+
+        return self
+
+    def __calculate_output(self):
+        if not self.full:
+            self.carryOut = And().set_input(0, self.A).set_input(1, self.B).get_output()
+            self.output = Xor().set_input(0, self.A).set_input(1, self.B).get_output()
+
+            return self
+
+        self.carryOut = Or(3).set_input(0, And().set_input(0, self.A).set_input(1, self.B).get_output())\
+            .set_input(1, And().set_input(0, self.A).set_input(1, self.carryIn).get_output())\
+            .set_input(2, And().set_input(0, self.B).set_input(1, self.carryIn).get_output())\
+            .get_output()
+
+        self.output = Xor(3).set_input(0, self.A).set_input(1, self.B).set_input(2, self.carryIn)
+
+        return self
