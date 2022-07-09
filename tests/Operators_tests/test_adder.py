131e8b979b1aa5df177eb39ff45ae802dd4b1f5e
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-10 01:11:36 +0200

    Implemented tests for full-adders

--- tests/Operators_tests/test_adder.py
+++ tests/Operators_tests/test_adder.py
@@ -30,3 +30,58 @@ def test_halfAdder():
         pass
 
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
+
+def test_fullAdder():
+    errors = []
+    half = Adder(True)
+
+    if half.get_output():
+        errors.append("0 + 0 != 1!")
+
+    if half.set_carryIn(True).get_output() and not half.get_carryOut():
+        pass
+    else:
+        errors.append("0 + 0 + 1 error!")
+
+    if half.set_carryIn(False).set_B(True).get_output() and not half.get_carryOut():
+        pass
+    else:
+        errors.append("0 + 1 + 0 error!")
+
+    if not half.set_carryIn(True).get_output() and half.get_carryOut():
+        pass
+    else:
+        errors.append("0 + 1 + 1 error!")
+
+    if half.set_carryIn(False).set_A(True).set_B(False).get_output() and not half.get_carryOut():
+        pass
+    else:
+        errors.append("1 + 0 + 0 error!")
+
+    if not half.set_carryIn(True).get_output() and half.get_carryOut():
+        pass
+    else:
+        errors.append("1 + 0 + 1 error!")
+
+    if not half.set_carryIn(False).set_B(True).get_output() and half.get_carryOut():
+        pass
+    else:
+        errors.append("1 + 1 + 0 error!")
+
+    if half.set_carryIn(True).get_output() and half.get_carryOut():
+        pass
+    else:
+        errors.append("1 + 1 + 1 error!")
+
+    try:
+        pass
+    except NotCorrectAdder:
+        half.set_carryIn(False)
+        errors.append("Should allow to set carryIn for a full-adder!")
+
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
+
+
+if __name__ == '__main__':
+    test_halfAdder()
+    test_fullAdder()
