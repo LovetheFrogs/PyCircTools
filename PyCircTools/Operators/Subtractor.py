8b05693a0e95ae5e0140a941942a28b3c3406483
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-11 22:09:05 +0200

    Added DocStrings

--- PyCircTools/Operators/Subtractor.py
+++ PyCircTools/Operators/Subtractor.py
@@ -2,11 +2,28 @@ from PyCircTools import Adder, Not, NotTruthValue
 
 
 class Subtractor(Adder):
+    """
+    Subtractor module. Inherits from Adder class because it just needs to negate the 'B' input.
+    """
     def __init__(self, full=False):
+        """
+        Subtractor class constructor. Calls the Adder constructor and then changes the value of the 'B' input.
+
+        :param full: Defaults to False. Used to specify if the type of adder used. (False -> Half-adder, True -> Full-adder).
+        :type full: bool
+        """
         super().__init__(full)
         self.B = Not().set_input(self.B).get_output()
 
     def set_B(self, value):
+        """
+        Method set_B needs to be overridden in order to set the 'B' input to the negation of the value so that it can
+        subtract.
+
+        :param value: Desired value of the adder's second summand.
+        :type value: bool
+        :raises NotTruthValue: Raised when a variable type is not bool.
+        """
         if type(value) is not bool:
             raise NotTruthValue
 
