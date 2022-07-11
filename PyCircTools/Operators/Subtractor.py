48c3a4b058f3bceb705b89de19867f1c3e73088f
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-11 22:04:15 +0200

    Overriden set_B method
    set_B method overriden so that input b will be not(value)

--- PyCircTools/Operators/Subtractor.py
+++ PyCircTools/Operators/Subtractor.py
@@ -1,7 +1,15 @@
-from PyCircTools import Adder, Not
+from PyCircTools import Adder, Not, NotTruthValue
 
 
 class Subtractor(Adder):
     def __init__(self, full=False):
         super().__init__(full)
         self.B = Not().set_input(self.B).get_output()
+
+    def set_B(self, value):
+        if type(value) is not bool:
+            raise NotTruthValue
+
+        self.B = Not().set_input(value).get_output()
+        super()._calculate_output()
+        return self
