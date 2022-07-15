1f5489543a63ff94b305a63b16ae7c578c5e63b1
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-15 14:46:05 +0200

    Fixed and completed module
    Fixed __calculate_output method
    
    - Shifter can now shift left or right
    - User can choose the number of positions to shift

--- PyCircTools/Operators/Shifter.py
+++ PyCircTools/Operators/Shifter.py
@@ -1,15 +1,24 @@
-from PyCircTools import DFlipflop, NotTruthValue
+from PyCircTools import DFlipflop, NotTruthValue, NotControlList, WrongSize
+import copy
 
 
 class Shifter:
-    def __init__(self, num=2):
+    def __init__(self, num=4, positions=1):
         self.num_of_inputs = num
+        self.positions = positions
+        self.D = False
         self.input = [False] * num
         self.clock = False
         self.output = self.__calculate_output()
 
     def get_numOfInputs(self):
         return self.num_of_inputs
+
+    def get_positions(self):
+        return self.positions
+
+    def get_D(self):
+        return self.D
 
     def get_input(self):
         return self.input
@@ -20,6 +29,14 @@ class Shifter:
     def get_clock(self):
         return self.clock
 
+    def set_D(self, value):
+        if type(value) is not bool:
+            raise NotTruthValue
+
+        self.D = value
+        self.output = self.__calculate_output()
+        return self
+
     def set_clock(self, value):
         if type(value) is not bool:
             raise NotTruthValue
@@ -29,8 +46,15 @@ class Shifter:
         return self
 
     def set_input(self, values):
-        if type(values) is not bool:
-            raise NotTruthValue
+        if type(values) is not list:
+            raise NotControlList
+
+        for val in values:
+            if type(val) is not bool:
+                raise NotControlList
+
+        if len(values) > self.num_of_inputs:
+            raise WrongSize(len(values), self.num_of_inputs)
 
         self.input = values
         self.output = self.__calculate_output()
@@ -39,7 +63,21 @@ class Shifter:
     def __calculate_output(self):
         output = [False] * self.num_of_inputs
         if self.clock:
-            for i in range(self.num_of_inputs):
-                output[i] = DFlipflop().set_D(self.input[i]).set_enable(True).get_Q()
+            output = copy.copy(self.input)
+            for _ in range(self.positions):
+                inp = copy.copy(output)
+                if not self.D:
+                    for i in range(self.num_of_inputs):
+                        if i == 0:
+                            output[i] = DFlipflop().set_D(inp[self.num_of_inputs - 1]).set_enable(True).get_Q()
+                        else:
+                            output[i] = DFlipflop().set_D(inp[i - 1]).set_enable(True).get_Q()
+
+                else:
+                    for i in range(self.num_of_inputs):
+                        if i == self.num_of_inputs - 1:
+                            output[i] = DFlipflop().set_D(inp[0]).set_enable(True).get_Q()
+                        else:
+                            output[i] = DFlipflop().set_D(inp[i + 1]).set_enable(True).get_Q()
 
         return output
