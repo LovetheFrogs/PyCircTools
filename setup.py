6477accddfe1ca18147882929e8dbe30c9edea18
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-07-17 17:23:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 
 this_directory = Path(__file__).parent
-desc = (this_directory / "PyPI_docs.md").read_text()
+desc = (this_directory / "README.md").read_text()
 setup(
     name='PyCircTools',
-    packages=find_packages(include=['Exceptions', 'LogicGates', 'Multiplexers', 'Latches']),
-    version='0.2.1',
-    description='PyCircTools, a python circuit library.',
+    packages=find_packages(include=['Exceptions', 'LogicGates', 'Multiplexers', 'Latches', 'Operators']),
+    version='1.0.0',
+    description='PyCircTools, a python library to build digital circuits.',
     author='LovetheFrogs',
     license='GPL-3.0',
-    url='https://github.com/LovetheFrogs/CTools',
+    url='https://github.com/LovetheFrogs/PyCircTools',
     long_description=desc,
     long_description_content_type='text/markdown'
 )
